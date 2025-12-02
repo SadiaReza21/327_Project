1c0517faaa23207360f7091e8eda180e139c59b3
Talha Muammar <talha.muammar.edu@gmail.com>
2025-12-02 20:00:22 +0600

    updated product schema. removed product create and update schema

--- schemas/product.py
+++ schemas/product.py
@@ -11,20 +11,6 @@ class ProductBase(BaseModel):
     stock: int
     image_url: Optional[str] = None
 
-
-class ProductCreate(ProductBase):
-    category_id: int
-
-
-class ProductUpdate(BaseModel):
-    name: str | None = None
-    description: str | None = None
-    price: float | None = None
-    stock: int | None = None
-    image_url: str | None = None
-    category_id: int | None = None
-
-
 class ProductResponse(ProductBase):
     product_id: int
     date_added: datetime
